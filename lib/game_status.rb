e733b6fc945dc182e8c43864dc0733ebed0a93d3
Carden Z <ephemere@protonmail.com>
2018-12-09 06:26:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,22 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+     x = win_combination.select do |index|
+           board[index] == win_combination[index]
+         end
+     x.length == 3 ? true : false
+  end
+end
